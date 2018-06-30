667ad9d8bddc298be7aa0b79b36564a4d0c83475
Andrew <santechserp@gmail.com>
2018-06-30 06:08:20 +0300

    решение императивный итеративный процесс

--- smallestDivisor.js
+++ smallestDivisor.js
@@ -1,4 +1,4 @@
-const smallestDivisor = (num) => {
+const smallestDivisor = (num) => {   //итеративным процессом решение
   if(num > 1){  //проверяет условие больше 1
 
     const small = (begin, end) => {
@@ -16,3 +16,29 @@ const smallestDivisor = (num) => {
     return num;
   }
 };
+
+// BEGIN (write your solution here)
+const smallestDivisor = (n) =>{  // императивный итеративный процесс
+  if(n < 1){
+    return NaN;
+  }else if(n == 1){
+    return n;
+  }else if(n > 1){
+     
+      let b = 1;
+      let c = 2;
+
+      while(b < n){
+         if (n % 2 === 0){
+            return 2;
+          }else if(n % 3 === 0){
+            return 3;
+          }else if(n % c ==0){
+            return c;
+          }
+        c += b;
+      }
+
+  }
+
+}
